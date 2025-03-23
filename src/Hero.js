775e983a38bad981e59cdce8020160e3e7e487f1
Hector J Fontanez <prof-fontanez@users.noreply.github.com>
2025-03-23 17:31:23 -0500

    Added navigation to the "Online menu" button

--- src/Hero.js
+++ src/Hero.js
@@ -2,12 +2,22 @@ import MenuSpecial from "./MenuSpecial";
 import bruchetta from "./assets/images/bruchetta(sm).jpg";
 import salad from "./assets/images/salad(sm).jpg";
 import dessert from "./assets/images/lemon_dessert.jpg";
+import { useNavigate } from 'react-router-dom';
 
-const Hero =  props => (
-    <div className="hero">
+const Hero =  props => {
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        navigate('/menu');
+    }
+
+    return(
+        <div className="hero">
         <div className="hero_container">
             <h2>This weeks specials!</h2>
-            <button className="button">Online menu</button>
+            <button className="button" onClick={(e) => handleSubmit(e)} >Online menu</button>
         </div>
         <div className="hero_container">
             <MenuSpecial image={bruchetta} price='$12.99' title='Bruchetta' description="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style Feta Cheese, garnished with crunchy garlic and Rosemary Croutons." />
@@ -15,6 +25,7 @@ const Hero =  props => (
             <MenuSpecial image={dessert} price='$5' title='Lemon Dessert' description="This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined." />
         </div>
     </div>
-);
+    );
+};
 
 export default Hero;
